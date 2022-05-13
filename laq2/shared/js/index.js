e15782ba4f88645d1455c134cf256becf8692dd5
b0ne <b0ne@b-and.co.jp>
2022-05-13 22:35:04 +0900

    top page, how to まで途中

--- laq2/shared/js/index.js
+++ laq2/shared/js/index.js
@@ -16,6 +16,40 @@ LAQtop.init = function() {
     });
   }
 
-  // sec feature scroll
+  // line up slick 
+  let lineupSlick = LAQtw.main.find('.sl-lineup').slick({
+    dots:false,
+    centerMode: true,
+    centerPadding: '80px',
+    slidesToShow: 2,
+
+    arrows: false,
+    autoplay: false,
+    infinite:false,
+    autoplaySpeed: 3000,
+    responsive: [
+      {
+        breakpoint:1200,
+        settings: {
+          centerPadding: '80px'
+        }
+      },
+      {
+        breakpoint:993,
+        settings: {
+          centerPadding:'40px'
+        }
+      },
+      {
+        breakpoint:768,
+        settings: {
+          slidesToShow: 1,
+          centerPadding:'30px'
+        }
+      }
+    ]
+  })
+  lineupSlick.slick('slickNext');
+
 }
 $(function(){ LAQtop.init() })
