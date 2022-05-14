18d6aa846ec024e98c82b7b81e72351e89f8eee8
b0ne <b0ne@b-and.co.jp>
2022-05-14 10:57:40 +0900

    top page faq content

--- laq2/shared/js/index.js
+++ laq2/shared/js/index.js
@@ -57,5 +57,24 @@ LAQtop.init = function() {
     lineupSlick.slick('slickNext');
   })
 
+  // faq qa 
+  let faq = $('main .faq-cont');
+  faq.find('.qa-a').slideUp(100);
+  faq.find('.qa-box .btn-tgg').on('click', function(){
+    let _this = $(this);
+    let _plus = _this.find('.plus');
+    let _min = _this.find('.minus');
+    let ans = _this.parents('.qa-box').find('.qa-a');
+    if (_plus.is(':visible')) {
+      _plus.hide();
+      _min.show();
+      ans.slideDown();
+    } else {
+      _plus.show();
+      _min.hide();
+      ans.slideUp();
+    }
+  })
+
 }
 $(function(){ LAQtop.init() })
