97143047a55adc34f18fc3fae48081ca51be7648
Okumura Kei <kei@iMac5K2017.local>
2022-06-02 03:42:10 +0900

    modified:トップページ| インジゲータProgressバー＆ズームアウトスライダー仕様開発[WIP]1

--- laq2/shared/js/index.js
+++ laq2/shared/js/index.js
@@ -1,3 +1,5 @@
+import { SetTopSlickSlider } from './setTopSlickSlider.js';
+
 let LAQtop = {};
 
 LAQtop.init = function() {
@@ -6,41 +8,6 @@ LAQtop.init = function() {
   self.slideSpeed = 1200;
   self.slidePause = 4000;
   new WOW().init();
-
-    // topSlide.bxSlider({
-    //   auto: true,
-    //   speed:self.slideSpeed,
-    //   pause:self.slidePause,
-    //   mode: 'fade',
-    //   infiniteLoop:true,
-    //   pager:false,
-    //   controls:false,
-    //   autoControls: false,
-    //   stopAuto:false,
-    //   onSliderLoad: function(index) {
-    //     if (index === 0) {
-    //       self.runCopyAnimate(1)
-    //     }
-    //   },
-    //   onSlideNext: function(element) {
-    //     let slItem = $(element);
-    //     if (slItem.find('.s01').length > 0) {
-    //       self.runCopyAnimate(1);
-    //     }
-    //     if (slItem.find('.s02').length > 0) {
-    //       self.runCopyAnimate(2);
-    //     }
-    //     if (slItem.find('.s03').length > 0) {
-    //       self.runCopyAnimate(3);
-    //     }
-    //     if (slItem.find('.s04').length > 0) {
-    //       self.runCopyAnimate(4);
-    //     }
-    //   },
-    //   onSlidePrev: function(element) {
-    //     // console.log('on slider prev : '+index);
-    //   }
-    // });
 
   // faq qa 
   let faq = $('main .faq-cont');
@@ -60,39 +27,27 @@ LAQtop.init = function() {
       ans.slideUp();
     }
   })
-
 }
 
-// LAQtop.runCopyAnimate = function(target) {
-//   let self = this;
-//   let topSlide = $('.t-slider');
-//   let CLASSNAME = "visibled";
-//   let TIMEOUT = self.slidePause - 500;
-//   let slCopyTarget = topSlide.find('.s0'+target+' .t-copy .tl');
 
-//   let hideTarget = ((target-1)==0) ? 4 : (target-1);
-//   topSlide.find('.s0'+hideTarget+' .t-copy .tl').removeClass(CLASSNAME);
-
-//   setTimeout(() => {
-//     slCopyTarget.addClass(CLASSNAME);
-//     // setTimeout(() => {
-//     //   slCopyTarget.removeClass(CLASSNAME);
-//     // }, TIMEOUT);
-//   }, self.slideSpeed/2);
-// }
 
 $(function(){ 
   
   LAQtop.init();
-  // set top slider: 
+
+  /*//////////////////////////////////////////////////////////////////////// Set top slider ////////////////////////////////////////////////////////*/
   const topSliderClassNameParams = '.t-slider'; // トップビジュアルのスライドを格納しているコンテンナを参照する時に必要
   const topSlidePrefixStringParams = '.s'; // .s01,.s02,.s03などを参照する時に必要
   const topSlideNumbersTextColorArrayParams = ['#1C1C1C','#1C1C1C','#1C1C1C','#fff']; //スライドが切替る時にインジケータの文字色を変更する時に参照
   const topSlideCopyTextColorArrayParams = ['#1C1C1C','#fff','#fff','#fff']; //各スライドのコピーや商品名の文字色を変更する時に参照
+  // let topCssStyle;
+  // let topSliderTime;//アニメーションの長さ（秒）//css変数(--top-slider-anim-time)から参照
+  // let topSlide, topSliderBar, topSliderTick, topSliderPercentTime;
+  new SetTopSlickSlider( topSliderClassNameParams, topSlidePrefixStringParams, topSlideNumbersTextColorArrayParams, topSlideCopyTextColorArrayParams); //トップビジュアルスライドの初期化
+  //SetTopSlickSlider( topSliderClassNameParams, topSlidePrefixStringParams, topSlideNumbersTextColorArrayParams, topSlideCopyTextColorArrayParams); //トップビジュアルスライドの初期化
 
-  SetTopSlickSlider(topSliderClassNameParams, topSlidePrefixStringParams, topSlideNumbersTextColorArrayParams, topSlideCopyTextColorArrayParams); //トップビジュアルスライドの初期化
+  /*//////////////////////////////////////////////////////////////////////// Set top slider ////////////////////////////////////////////////////////*/
 
-//window.onload = function(){
   // line up slick 
   let lineupCont = LAQtw.main.find('.lineup-cont');
   let lineupSlick = lineupCont.find('.sl-lineup').slick({
@@ -141,7 +96,8 @@ $(function(){
     $beforeButton.css('left', margin).css('top', height);
   }
 
-  lineupSlick.on('beforeChange', function(_, slick, _, nextSlide){
+  // lineupSlick.on('beforeChange', function(_, slick, _, nextSlide){
+    lineupSlick.on('beforeChange', function(event, slick, cs, nextSlide){
     const totalCount = slick.slideCount;
     const slidesToShow = slick.options.slidesToShow
     const currentSlide = nextSlide + 1;
@@ -179,153 +135,197 @@ $(function(){
     lineupSlick.slick('slickNext');
   })
 
-//}
-
-})
-
 
 // TOP VISUAL SLIDER //////////////////////////////////////////////////////////////////////////////////////////////////////////
-function SetTopSlickSlider(_sliderclassname, _slideprefix, _topslidenumberstextcolorarray, _topslidecopytextcolorarray){
-  let sliderClassName = _sliderclassname;
-  let slidePrefixString = _slideprefix;
-  let topSlide = $(sliderClassName);
-  let topSlideNumbersTextColorArray = _topslidenumberstextcolorarray;
-  let topSlideCopyTextColorArray = _topslidecopytextcolorarray;
-  let topSlideDotsContainerText = $('.slide-dots-container span');
-  let topSlideDotsContainerCurrentCountText = $('.slide-dots-container-current-number span');
-  let topSlideDotsContainerTotalCountText = $('.slide-dots-container-total-number span');
-  let topSlideCurrentNumber;
 
-  if (topSlide.length > 0){
-    //set font color for copies from array
-    let slideInnerClasses = sliderClassName + ' .slide-inner .sl-cont';
-    let slideItems = $(slideInnerClasses);
-    slideItems.each(function(i){ $(this).css('color', topSlideCopyTextColorArray[i])})
+// function SetTopSlickSlider(_sliderclassname, _slideprefix, _topslidenumberstextcolorarray, _topslidecopytextcolorarray){
+//   let sliderClassName = _sliderclassname;
+//   let slidePrefixString = _slideprefix;
+//   topSlide = $(sliderClassName);
+//   let topSlideNumbersTextColorArray = _topslidenumberstextcolorarray;
+//   let topSlideCopyTextColorArray = _topslidecopytextcolorarray;
+//   let topSlideDotsContainerText = $('.slide-dots-container span');
+//   let topSlideDotsContainerCurrentCountText = $('.slide-dots-container-current-number span');
+//   let topSlideDotsContainerTotalCountText = $('.slide-dots-container-total-number span');
+//   let topSlideCurrentNumber;
+//   topCssStyle = getComputedStyle(document.body);
+//   topSliderTime = topCssStyle.getPropertyValue('--top-slider-anim-time').slice(0, -1); //アニメーションの長さ（秒）//css変数(--top-slider-anim-time)から参照
 
-    topSlide.on('init', function(e, slick, direction){
-      const selectedSliderClassName = slidePrefixString + "01";
-      const prevSliderClassName = slidePrefixString + ('0' + parseInt(slick.slideCount)).slice(-2);
-      topSlidePrevItemsClassStrings = prevSliderClassName + ' .sl-cont .t-copy .tl';
-      topSlideItemsClassStrings = selectedSliderClassName + ' .sl-cont .t-copy .tl';
-      const topSlideItems = $(topSlideItemsClassStrings);
-      const topPrevSlideItems = $(topSlidePrevItemsClassStrings);
-      topSlideCurrentNumber = 1;
-      topSlideDotsContainerText.css('color', topSlideNumbersTextColorArray[topSlideCurrentNumber]);
-      topSlideDotsContainerTotalCountText.text(('0' + slick.slideCount).slice(-2));
-      topSlideDotsContainerCurrentCountText.text(('0' + topSlideCurrentNumber).slice(-2));
-      topPrevSlideItems.each(function(){ $(this).removeClass('visibled')});
-      topSlideItems.each(function(){ $(this).addClass('visibled')});
-    });
+//   topSliderBar = $('.slide-dots-container-slider-progress-bar');
 
-    topSlide.on('beforeChange', function(e, slick, currentSlide, nextSlide){
-      // topSlide.slick('slickPause');
-      let selectedItem = nextSlide;
-      const selectedSliderClassName = slidePrefixString + ('0' + parseInt(selectedItem + 1)).slice(-2);
-      const prevSliderClassName = slidePrefixString + ('0' + parseInt(selectedItem)).slice(-2);
-      const topSlideItemBGImage = $(prevSliderClassName + ' .sl-bg');
-      const topSlideItemBGImageNext = $(selectedSliderClassName + ' .sl-bg');
-      const topSlideItemBGImageSP = $(prevSliderClassName + ' .sl-bg-sp');
-      const topSlideItemBGImageNextSP = $(selectedSliderClassName + ' .sl-bg-sp');
+//   if (topSlide.length > 0){
+//     //set font color for copies from array
+//     let slideInnerClasses = sliderClassName + ' .slide-inner .sl-cont';
+//     let slideItems = $(slideInnerClasses);
+//     slideItems.each(function(i){ $(this).css('color', topSlideCopyTextColorArray[i])})
 
-      // console.log(topSlideItemBGImage.attr('class'));
-      topSlideItemBGImage.css({
-        '-webkit-transform' : 'scale(1.1)',
-        '-moz-transform'    : 'scale(1.1)',
-        '-ms-transform'     : 'scale(1.1)',
-        '-o-transform'      : 'scale(1.1)',
-        'transform'         : 'scale(1.1)'
-      });
-      topSlideItemBGImageNext.css({
-        '-webkit-transform' : 'scale(1)',
-        '-moz-transform'    : 'scale(1)',
-        '-ms-transform'     : 'scale(1)',
-        '-o-transform'      : 'scale(1)',
-        'transform'         : 'scale(1)'
-      });
-      topSlideItemBGImageSP.css({
-        '-webkit-transform' : 'scale(1.1)',
-        '-moz-transform'    : 'scale(1.1)',
-        '-ms-transform'     : 'scale(1.1)',
-        '-o-transform'      : 'scale(1.1)',
-        'transform'         : 'scale(1.1)'
-      });
-      topSlideItemBGImageNextSP.css({
-        '-webkit-transform' : 'scale(1)',
-        '-moz-transform'    : 'scale(1)',
-        '-ms-transform'     : 'scale(1)',
-        '-o-transform'      : 'scale(1)',
-        'transform'         : 'scale(1)'
-      });
-      topSlideItemsClassStrings = selectedSliderClassName + ' .sl-cont .t-copy .tl';
-      topSlidePrevItemsClassStrings = prevSliderClassName + ' .sl-cont .t-copy .tl';
-      const topSlideItems = $(topSlideItemsClassStrings);
-      const topPrevSlideItems = $(topSlidePrevItemsClassStrings);
-      topSlideCurrentNumber = selectedItem;
-      topSlideDotsContainerText.css('color', topSlideNumbersTextColorArray[topSlideCurrentNumber]);
-      (currentSlide >= slick.slideCount) ? topSlideCurrentNumber = 1 : topSlideCurrentNumber = parseInt(selectedItem + 1);
-      if(currentSlide >= (slick.slideCount - 1)){
-        const topSlideLastItemsClassName = slidePrefixString + ('0' + parseInt(slick.slideCount)).slice(-2);
-        const topSlideLastItemsClassStrings = topSlideLastItemsClassName + ' .sl-cont .t-copy .tl';
-        const topLastSlideItems = $(topSlideLastItemsClassStrings);
-        topLastSlideItems.each(function(){ $(this).removeClass('visibled')});
-        const topSlideItemBGImageLast= $(topSlideLastItemsClassName + ' .sl-bg');
-        const topSlideItemBGImageLastSP= $(topSlideLastItemsClassName + ' .sl-bg-sp');
-        topSlideItemBGImageLast.css({
-          '-webkit-transform' : 'scale(1)',
-          '-moz-transform'    : 'scale(1)',
-          '-ms-transform'     : 'scale(1)',
-          '-o-transform'      : 'scale(1)',
-          'transform'         : 'scale(1)'
-        });
-        topSlideItemBGImageLastSP.css({
-          '-webkit-transform' : 'scale(1)',
-          '-moz-transform'    : 'scale(1)',
-          '-ms-transform'     : 'scale(1)',
-          '-o-transform'      : 'scale(1)',
-          'transform'         : 'scale(1)'
-        });
-      }
-      topSlideDotsContainerCurrentCountText.text(('0' + topSlideCurrentNumber).slice(-2));
-      topPrevSlideItems.each(function(){$(this).removeClass('visibled')});
-      topSlideItems.each(function(){$(this).addClass('visibled')});
-    });
+//     topSlide.on('init', function(e, slick, direction){
+//       const selectedSliderClassName = slidePrefixString + "01";
+//       const prevSliderClassName = slidePrefixString + ('0' + parseInt(slick.slideCount)).slice(-2);
+//       const topSlidePrevItemsClassStrings = prevSliderClassName + ' .sl-cont .t-copy .tl';
+//       const topSlideItemsClassStrings = selectedSliderClassName + ' .sl-cont .t-copy .tl';
+//       const topSlideItems = $(topSlideItemsClassStrings);
+//       const topPrevSlideItems = $(topSlidePrevItemsClassStrings);
+//       topSlideCurrentNumber = 1;
+//       topSlideDotsContainerText.css('color', topSlideNumbersTextColorArray[topSlideCurrentNumber]);
+//       topSlideDotsContainerTotalCountText.text(('0' + slick.slideCount).slice(-2));
+//       topSlideDotsContainerCurrentCountText.text(('0' + topSlideCurrentNumber).slice(-2));
+//       topPrevSlideItems.each(function(){ $(this).removeClass('visibled')});
+//       topSlideItems.each(function(){ $(this).addClass('visibled')});
+//     });
+
+//     topSlide.on('beforeChange', function(e, slick, currentSlide, nextSlide){
+//       // topSlide.slick('slickPause');
+//       let selectedItem = nextSlide;
+//       const selectedSliderClassName = slidePrefixString + ('0' + parseInt(selectedItem + 1)).slice(-2);
+//       const prevSliderClassName = slidePrefixString + ('0' + parseInt(selectedItem)).slice(-2);
+//       const topSlideItemBGImage = $(prevSliderClassName + ' .sl-bg');
+//       const topSlideItemBGImageNext = $(selectedSliderClassName + ' .sl-bg');
+//       const topSlideItemBGImageSP = $(prevSliderClassName + ' .sl-bg-sp');
+//       const topSlideItemBGImageNextSP = $(selectedSliderClassName + ' .sl-bg-sp');
+
+//       resetProgressbar();
+//       startProgressbar();
+//       topSliderBar.css({
+//           height: 100 + "%"
+//       });
+
+//       // console.log(topSlideItemBGImage.attr('class'));
+//       topSlideItemBGImage.css({
+//         '-webkit-transform' : 'scale(1.1)',
+//         '-moz-transform'    : 'scale(1.1)',
+//         '-ms-transform'     : 'scale(1.1)',
+//         '-o-transform'      : 'scale(1.1)',
+//         'transform'         : 'scale(1.1)'
+//       });
+//       topSlideItemBGImageNext.css({
+//         '-webkit-transform' : 'scale(1)',
+//         '-moz-transform'    : 'scale(1)',
+//         '-ms-transform'     : 'scale(1)',
+//         '-o-transform'      : 'scale(1)',
+//         'transform'         : 'scale(1)'
+//       });
+//       topSlideItemBGImageSP.css({
+//         '-webkit-transform' : 'scale(1.1)',
+//         '-moz-transform'    : 'scale(1.1)',
+//         '-ms-transform'     : 'scale(1.1)',
+//         '-o-transform'      : 'scale(1.1)',
+//         'transform'         : 'scale(1.1)'
+//       });
+//       topSlideItemBGImageNextSP.css({
+//         '-webkit-transform' : 'scale(1)',
+//         '-moz-transform'    : 'scale(1)',
+//         '-ms-transform'     : 'scale(1)',
+//         '-o-transform'      : 'scale(1)',
+//         'transform'         : 'scale(1)'
+//       });
+//       const topSlideItemsClassStrings = selectedSliderClassName + ' .sl-cont .t-copy .tl';
+//       const topSlidePrevItemsClassStrings = prevSliderClassName + ' .sl-cont .t-copy .tl';
+//       const topSlideItems = $(topSlideItemsClassStrings);
+//       const topPrevSlideItems = $(topSlidePrevItemsClassStrings);
+//       topSlideCurrentNumber = selectedItem;
+//       topSlideDotsContainerText.css('color', topSlideNumbersTextColorArray[topSlideCurrentNumber]);
+//       (currentSlide >= slick.slideCount) ? topSlideCurrentNumber = 1 : topSlideCurrentNumber = parseInt(selectedItem + 1);
+//       if(currentSlide >= (slick.slideCount - 1)){
+//         const topSlideLastItemsClassName = slidePrefixString + ('0' + parseInt(slick.slideCount)).slice(-2);
+//         const topSlideLastItemsClassStrings = topSlideLastItemsClassName + ' .sl-cont .t-copy .tl';
+//         const topLastSlideItems = $(topSlideLastItemsClassStrings);
+//         topLastSlideItems.each(function(){ $(this).removeClass('visibled')});
+//         const topSlideItemBGImageLast= $(topSlideLastItemsClassName + ' .sl-bg');
+//         const topSlideItemBGImageLastSP= $(topSlideLastItemsClassName + ' .sl-bg-sp');
+//         topSlideItemBGImageLast.css({
+//           '-webkit-transform' : 'scale(1)',
+//           '-moz-transform'    : 'scale(1)',
+//           '-ms-transform'     : 'scale(1)',
+//           '-o-transform'      : 'scale(1)',
+//           'transform'         : 'scale(1)'
+//         });
+//         topSlideItemBGImageLastSP.css({
+//           '-webkit-transform' : 'scale(1)',
+//           '-moz-transform'    : 'scale(1)',
+//           '-ms-transform'     : 'scale(1)',
+//           '-o-transform'      : 'scale(1)',
+//           'transform'         : 'scale(1)'
+//         });
+//       }
+//       topSlideDotsContainerCurrentCountText.text(('0' + topSlideCurrentNumber).slice(-2));
+//       topPrevSlideItems.each(function(){$(this).removeClass('visibled')});
+//       topSlideItems.each(function(){$(this).addClass('visibled')});
+//     });
+
     
-    topSlide.slick({
-      dots: true,
-      infinite: true,
-      speed: 500,
-      fade: true,
-      cssEase: 'linear',
-      autoplay: true,
-      autoplaySpeed: 5000,
-      accessibility: false,
-      dotsClass: 'slide-dots',
-      appendDots: $('.slide-dots-container-dots'),
-      pauseOnFocus: false,
-      pauseOnHover: false,
-      pauseOnDotsHover: false,
-      accesibility: false,
-      draggable: false,
-      swipe: false,
-      touchMove: false,
+//     topSlide.slick({
+//       dots: true,
+//       infinite: true,
+//       fade: true,
+//       cssEase: 'linear',
+//       autoplay: true,
+//       // speed: 500,
+//       // autoplaySpeed: 5000,
+//       accessibility: false,
+//       dotsClass: 'slide-dots',
+//       appendDots: $('.slide-dots-container-dots'),
+//       pauseOnFocus: false,
+//       pauseOnHover: false,
+//       pauseOnDotsHover: false,
+//       accesibility: false,
+//       draggable: false,
+//       swipe: false,
+//       touchMove: false,
+//       arrows: false,
+//       slidesToShow: 1,
+//       slidesToScroll: 1,
 
-      responsive: [
-        {
-          breakpoint: 767,
-          settings: {
-            centerMode: false,
-          }
-        },
-      ],
+//       responsive: [
+//         {
+//           breakpoint: 767,
+//           settings: {
+//             centerMode: false,
+//           }
+//         },
+//       ],
 
-    });
+//     });
 
-    $('.slick-dots li button').on('click', function(e){
-      e.stopPropagation();
-    });
-    // $(window).on('resize orientationchange', function() {
-    //    topSlide.slick('resize');
-    // });
-    }
-}
+//     topSlide.slick('slickPause');
+//     startProgressbar(topSlide);
+    
+//     //topSlide.slick('slickPlay');
+
+
+//     $('.slick-dots li button').on('click', function(e){
+//       e.stopPropagation();
+//     });
+//     // $(window).on('resize orientationchange', function() {
+//     //    topSlide.slick('resize');
+//     // });
+//     }
+// }
+
+// function startProgressbar() {
+//     resetProgressbar();
+//     topSliderPercentTime = 0;
+//     topSliderTick = setInterval(interval, 10);
+//     console.log(topSliderTime);
+// }
+
+// function interval() {
+//     topSliderPercentTime += 1 / (Number(topSliderTime - .5) + 0.1);
+//     topSliderBar.css({
+//         height: topSliderPercentTime + "%"
+//     });
+//     if (topSliderPercentTime >= 100) {
+//         topSlide.slick('slickNext');
+//         startProgressbar();
+//     }
+// }
+
+// function resetProgressbar() {
+//     topSliderBar.css({
+//         height: 0 + '%'
+//     });
+//     clearTimeout(topSliderTick);
+// }
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+});
---
+++ laq2/shared/js/setTopSlickSlider.js
@@ -0,0 +1,196 @@
+// export function SetTopSlickSlider(){
+//     console.log('hello!');
+// }
+export let topCssStyle;
+export let topSliderTime;//アニメーションの長さ（秒）//css変数(--top-slider-anim-time)から参照
+export let topSlide, topSliderBar, topSliderTick, topSliderPercentTime;
+
+export function SetTopSlickSlider(_sliderclassname, _slideprefix, _topslidenumberstextcolorarray, _topslidecopytextcolorarray){
+  let sliderClassName = _sliderclassname;
+  let slidePrefixString = _slideprefix;
+  topSlide = $(sliderClassName);
+  let topSlideNumbersTextColorArray = _topslidenumberstextcolorarray;
+  let topSlideCopyTextColorArray = _topslidecopytextcolorarray;
+  let topSlideDotsContainerText = $('.slide-dots-container span');
+  let topSlideDotsContainerCurrentCountText = $('.slide-dots-container-current-number span');
+  let topSlideDotsContainerTotalCountText = $('.slide-dots-container-total-number span');
+  let topSlideCurrentNumber;
+  topCssStyle = getComputedStyle(document.body);
+  topSliderTime = topCssStyle.getPropertyValue('--top-slider-anim-time').slice(0, -1); //アニメーションの長さ（秒）//css変数(--top-slider-anim-time)から参照
+
+  topSliderBar = $('.slide-dots-container-slider-progress-bar');
+
+  if (topSlide.length > 0){
+    //set font color for copies from array
+    let slideInnerClasses = sliderClassName + ' .slide-inner .sl-cont';
+    let slideItems = $(slideInnerClasses);
+    slideItems.each(function(i){ $(this).css('color', topSlideCopyTextColorArray[i])})
+
+    topSlide.on('init', function(e, slick, direction){
+      const selectedSliderClassName = slidePrefixString + "01";
+      const prevSliderClassName = slidePrefixString + ('0' + parseInt(slick.slideCount)).slice(-2);
+      const topSlidePrevItemsClassStrings = prevSliderClassName + ' .sl-cont .t-copy .tl';
+      const topSlideItemsClassStrings = selectedSliderClassName + ' .sl-cont .t-copy .tl';
+      const topSlideItems = $(topSlideItemsClassStrings);
+      const topPrevSlideItems = $(topSlidePrevItemsClassStrings);
+      topSlideCurrentNumber = 1;
+      topSlideDotsContainerText.css('color', topSlideNumbersTextColorArray[topSlideCurrentNumber]);
+      topSlideDotsContainerTotalCountText.text(('0' + slick.slideCount).slice(-2));
+      topSlideDotsContainerCurrentCountText.text(('0' + topSlideCurrentNumber).slice(-2));
+      topPrevSlideItems.each(function(){ $(this).removeClass('visibled')});
+      topSlideItems.each(function(){ $(this).addClass('visibled')});
+    });
+
+    topSlide.on('beforeChange', function(e, slick, currentSlide, nextSlide){
+      // topSlide.slick('slickPause');
+      let selectedItem = nextSlide;
+      const selectedSliderClassName = slidePrefixString + ('0' + parseInt(selectedItem + 1)).slice(-2);
+      const prevSliderClassName = slidePrefixString + ('0' + parseInt(selectedItem)).slice(-2);
+      const topSlideItemBGImage = $(prevSliderClassName + ' .sl-bg');
+      const topSlideItemBGImageNext = $(selectedSliderClassName + ' .sl-bg');
+      const topSlideItemBGImageSP = $(prevSliderClassName + ' .sl-bg-sp');
+      const topSlideItemBGImageNextSP = $(selectedSliderClassName + ' .sl-bg-sp');
+
+      resetProgressbar();
+      startProgressbar();
+      topSliderBar.css({
+          height: 100 + "%"
+      });
+
+      // console.log(topSlideItemBGImage.attr('class'));
+      topSlideItemBGImage.css({
+        '-webkit-transform' : 'scale(1.1)',
+        '-moz-transform'    : 'scale(1.1)',
+        '-ms-transform'     : 'scale(1.1)',
+        '-o-transform'      : 'scale(1.1)',
+        'transform'         : 'scale(1.1)'
+      });
+      topSlideItemBGImageNext.css({
+        '-webkit-transform' : 'scale(1)',
+        '-moz-transform'    : 'scale(1)',
+        '-ms-transform'     : 'scale(1)',
+        '-o-transform'      : 'scale(1)',
+        'transform'         : 'scale(1)'
+      });
+      topSlideItemBGImageSP.css({
+        '-webkit-transform' : 'scale(1.1)',
+        '-moz-transform'    : 'scale(1.1)',
+        '-ms-transform'     : 'scale(1.1)',
+        '-o-transform'      : 'scale(1.1)',
+        'transform'         : 'scale(1.1)'
+      });
+      topSlideItemBGImageNextSP.css({
+        '-webkit-transform' : 'scale(1)',
+        '-moz-transform'    : 'scale(1)',
+        '-ms-transform'     : 'scale(1)',
+        '-o-transform'      : 'scale(1)',
+        'transform'         : 'scale(1)'
+      });
+      const topSlideItemsClassStrings = selectedSliderClassName + ' .sl-cont .t-copy .tl';
+      const topSlidePrevItemsClassStrings = prevSliderClassName + ' .sl-cont .t-copy .tl';
+      const topSlideItems = $(topSlideItemsClassStrings);
+      const topPrevSlideItems = $(topSlidePrevItemsClassStrings);
+      topSlideCurrentNumber = selectedItem;
+      topSlideDotsContainerText.css('color', topSlideNumbersTextColorArray[topSlideCurrentNumber]);
+      (currentSlide >= slick.slideCount) ? topSlideCurrentNumber = 1 : topSlideCurrentNumber = parseInt(selectedItem + 1);
+      if(currentSlide >= (slick.slideCount - 1)){
+        const topSlideLastItemsClassName = slidePrefixString + ('0' + parseInt(slick.slideCount)).slice(-2);
+        const topSlideLastItemsClassStrings = topSlideLastItemsClassName + ' .sl-cont .t-copy .tl';
+        const topLastSlideItems = $(topSlideLastItemsClassStrings);
+        topLastSlideItems.each(function(){ $(this).removeClass('visibled')});
+        const topSlideItemBGImageLast= $(topSlideLastItemsClassName + ' .sl-bg');
+        const topSlideItemBGImageLastSP= $(topSlideLastItemsClassName + ' .sl-bg-sp');
+        topSlideItemBGImageLast.css({
+          '-webkit-transform' : 'scale(1)',
+          '-moz-transform'    : 'scale(1)',
+          '-ms-transform'     : 'scale(1)',
+          '-o-transform'      : 'scale(1)',
+          'transform'         : 'scale(1)'
+        });
+        topSlideItemBGImageLastSP.css({
+          '-webkit-transform' : 'scale(1)',
+          '-moz-transform'    : 'scale(1)',
+          '-ms-transform'     : 'scale(1)',
+          '-o-transform'      : 'scale(1)',
+          'transform'         : 'scale(1)'
+        });
+      }
+      topSlideDotsContainerCurrentCountText.text(('0' + topSlideCurrentNumber).slice(-2));
+      topPrevSlideItems.each(function(){$(this).removeClass('visibled')});
+      topSlideItems.each(function(){$(this).addClass('visibled')});
+    });
+
+    
+    topSlide.slick({
+      dots: true,
+      infinite: true,
+      fade: true,
+      cssEase: 'linear',
+      autoplay: true,
+      // speed: 500,
+      // autoplaySpeed: 5000,
+      accessibility: false,
+      dotsClass: 'slide-dots',
+      appendDots: $('.slide-dots-container-dots'),
+      pauseOnFocus: false,
+      pauseOnHover: false,
+      pauseOnDotsHover: false,
+      accesibility: false,
+      draggable: false,
+      swipe: false,
+      touchMove: false,
+      arrows: false,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+
+      responsive: [
+        {
+          breakpoint: 767,
+          settings: {
+            centerMode: false,
+          }
+        },
+      ],
+
+    });
+
+    topSlide.slick('slickPause');
+    startProgressbar(topSlide);
+    
+    //topSlide.slick('slickPlay');
+
+
+    $('.slick-dots li button').on('click', function(e){
+      e.stopPropagation();
+    });
+    // $(window).on('resize orientationchange', function() {
+    //    topSlide.slick('resize');
+    // });
+    }
+}
+
+
+export function startProgressbar() {
+    resetProgressbar();
+    topSliderPercentTime = 0;
+    topSliderTick = setInterval(interval, 10);
+    // console.log(topSliderTime);
+}
+
+export function interval() {
+    topSliderPercentTime += 1 / (Number(topSliderTime - .5) + 0.1);
+    topSliderBar.css({
+        height: topSliderPercentTime + "%"
+    });
+    if (topSliderPercentTime >= 100) {
+        topSlide.slick('slickNext');
+        startProgressbar();
+    }
+}
+
+export function resetProgressbar() {
+    topSliderBar.css({
+        height: 0 + '%'
+    });
+    clearTimeout(topSliderTick);
+}
